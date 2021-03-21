hicknhack-software/Qt-Qbs

prevent the plugins path from doubling up. (feels hacky, but works for now)
There are two call sites. If the path comes from the build environment of QtCreator the path is wong.

src/lib/corelib/tools/preferences.cpp
@@ -121,7 +121,7 @@ QStringList Preferences::searchPaths(const QString &baseDir) const
  */
 QStringList Preferences::pluginPaths(const QString &baseDir) const
 {
-    return pathList(QLatin1String("pluginsPath"), baseDir + QLatin1String("/qbs/plugins"));
+    return pathList(QLatin1String("pluginsPath"), (baseDir.endsWith(QLatin1String("/qbs/plugins")) || baseDir.endsWith(QLatin1String("\\qbs\\plugins"))) ? baseDir : (baseDir + QLatin1String("/qbs/plugins")));
 }
 
 QVariant Preferences::getPreference(const QString &key, const QVariant &defaultValue) const

arBmind/qbs

src/lib/corelib/tools/preferences.cpp
@@ -122,7 +122,7 @@ QStringList Preferences::searchPaths(const QString &baseDir) const
  */
 QStringList Preferences::pluginPaths(const QString &baseDir) const
 {
-    return pathList(QStringLiteral("pluginsPath"), baseDir + QStringLiteral("/qbs/plugins"));
+    return pathList(QStringLiteral("pluginsPath"), (baseDir.endsWith(QStringLiteral("/qbs/plugins")) || baseDir.endsWith(QStringLiteral("\\qbs\\plugins"))) ? baseDir : (baseDir + QStringLiteral("/qbs/plugins")));
 }
 
 /*!